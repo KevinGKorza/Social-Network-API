ef4caa29047edb73f9f4aa04707ed5b09d0618b6
kevinkorza <kevingkorza@gmail.com>
2022-11-07 14:59:41 -0500

    utils update

--- utils/data.js
+++ utils/data.js
@@ -1,41 +1,41 @@
-const names = [
-    'Homer',
-    'Marge',
-    'Bart',
-    'Lisa'
-  ];
+// const names = [
+//     'Homer',
+//     'Marge',
+//     'Bart',
+//     'Lisa'
+//   ];
   
-  const randomTasks = [
-    'Go to work',
-    'Prank someone',
-    'Clean the house',
-    'Feed the dog',
-    'Feed the cat',
-    'Watch Maggie',
-    'Go to Moes',
-    'Play the Sax',
-    'Go to a baseball game'
-  ];
+//   const randomTasks = [
+//     'Go to work',
+//     'Prank someone',
+//     'Clean the house',
+//     'Feed the dog',
+//     'Feed the cat',
+//     'Watch Maggie',
+//     'Go to Moes',
+//     'Play the Sax',
+//     'Go to a baseball game'
+//   ];
   
-  // The code to get a random array item
-  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+//   // The code to get a random array item
+//   const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
   
-  // The code that gets a random name from above
-  const getName = () =>
-    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+//   // The code that gets a random name from above
+//   const getName = () =>
+//     `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
   
-  // The code to generate random tasks (from above)
-  const getRandomTasks = (int) => {
-    const results = [];
-    for (let i = 0; i < int; i++) {
-      results.push({
-        taskName: getRandomArrItem(randomTasks),
-        score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-      });
-    }
-    return results;
-  };
+//   // The code to generate random tasks (from above)
+//   const getRandomTasks = (int) => {
+//     const results = [];
+//     for (let i = 0; i < int; i++) {
+//       results.push({
+//         taskName: getRandomArrItem(randomTasks),
+//         score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+//       });
+//     }
+//     return results;
+//   };
   
 
-  module.exports = { getName, getRandomTasks };
+//   module.exports = { getName, getRandomTasks };
   
--- utils/seed.js
+++ utils/seed.js
@@ -1,46 +1,46 @@
-const connection = require('../config/connection');
-const { thought, user } = require('../models');
-const { getRandomName, getRandomTasks } = require('./data');
+// const connection = require('../config/connection');
+// const { thought, user } = require('../models');
+// const { getRandomName, getRandomTasks } = require('./data');
 
-connection.on('error', (err) => err);
+// connection.on('error', (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+// connection.once('open', async () => {
+//   console.log('connected');
 
-  // Drop existing thoughts
-  await thought.deleteMany({});
+//   // Drop existing thoughts
+//   await thought.deleteMany({});
 
-  // Drop existing user
-  await user.deleteMany({});
+//   // Drop existing user
+//   await user.deleteMany({});
 
-  // Create empty array to hold the users
-  const user = [];
+//   // Create empty array to hold the users
+//   const user = [];
 
 
-  for (let i = 0; i < 20; i++) {
+//   for (let i = 0; i < 20; i++) {
     
-    const tasks = getRandomTasks(20);
+//     const tasks = getRandomTasks(20);
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
+//     const fullName = getRandomName();
+//     const first = fullName.split(' ')[0];
     
 
-    user.push({
-      first,
-      tasks,
-    });
-  }
+//     user.push({
+//       first,
+//       tasks,
+//     });
+//   }
 
-  // Add user to the collection and await the results
-  await user.collection.insertMany(user);
+//   // Add user to the collection and await the results
+//   await user.collection.insertMany(user);
 
-  // Add thoughts to the collection and await the results
-  await thought.collection.insertOne({
+//   // Add thoughts to the collection and await the results
+//   await thought.collection.insertOne({
   
-  });
+//   });
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(user);
-  console.info('Seeding complete! 🌱');
-  process.exit(0);
-});
+//   // Log out the seed data to indicate what should appear in the database
+//   console.table(user);
+//   console.info('Seeding complete! 🌱');
+//   process.exit(0);
+// });
